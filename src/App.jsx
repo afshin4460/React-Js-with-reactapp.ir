e021cfaa759be41516870b2b46ab51f18f068851
Afshin <afshin.m4460@gmail.com>
2023-02-18 01:09:56 +0330

    create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Website from './components/webstie';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello</h1>
+      <Website />
     </div>
   );
 }
---
+++ src/components/webstie.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Welcome() {
+    return (
+        <h1>welcome tor React world developer</h1>
+    );
+}
+
+function Website() {
+    return (
+        <div>
+            <Welcome />
+            <h2>reactapp.ir</h2>
+        </div>
+    );
+}
+
+export default Website;
