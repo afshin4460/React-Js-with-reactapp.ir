a3b624a9fb2c17d785235465ba7cbc5a810699fd
Afshin <afshin.m4460@gmail.com>
2023-02-18 01:27:02 +0330

    passing props to components

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ import './App.css';
 import Website from './components/webstie';
 
 function App() {
+
+  const handleClick = (name) => {
+    alert('Hi ' + name)
+  };
+
   return (
     <div className='App'>
-      <Website />
+      <Website name='Afshin' age='28' click={() => handleClick('Afshin')} />
+      <Website name='shiva' age='27' click={() => handleClick('Shiva')} />
     </div>
   );
 }
--- src/components/webstie.jsx
+++ src/components/webstie.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 
-function Welcome() {
+function Welcome(props) {
     return (
-        <h1>welcome tor React world developer</h1>
+        <div>
+            {props.children}
+        </div>
     );
 }
 
-function Website() {
+function Website(props) {
     return (
         <div>
-            <Welcome />
+            <Welcome>
+                <h1>welcome tor React world developer</h1>
+            </Welcome>
             <h2>reactapp.ir</h2>
+            <p onClick={props.click}>My name is {props.name} and {props.age} years old</p>
         </div>
     );
 }
