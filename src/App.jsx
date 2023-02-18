b522e7e91dcd0cd35989166f65f493c661cdf28b
Afshin <afshin.m4460@gmail.com>
2023-02-18 14:18:29 +0330

    conditional rendering 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,24 @@ import React, {useState} from 'react';
 import './App.css';
 //import Website from './components/webstie';
 //import Counter from './components/counter/counter';
+import Greeting from './components/greeting';
 
 function App() {
 
   /* const handleClick = (name) => { //for Website component
     alert('Hi ' + name)
   }; */
-  const actionLink = (e) => {
+  /* const actionLink = (e) => {
     e.preventDefault();
     alert('clicked');
   };
   const [isToggleOn, setIsToggleOn] = useState(true);
   const handleToggleClick = () => {
     setIsToggleOn(!isToggleOn);
+  }; */
+  const [isLogedIn, setIsLogedIn] = useState(true);
+  const handleLoginClick = () => {
+    setIsLogedIn(!isLogedIn);
   };
 
   return (
@@ -22,8 +27,10 @@ function App() {
       {/* <Website name='Afshin' age={28} click={() => handleClick('Afshin')} />
       <Website name='shiva' age={27} click={() => handleClick('Shiva')} /> */}
       {/* <Counter /> */}
-      <a href="https://reactapp.ir" onClick={actionLink}>click me</a>
-      <button onClick={handleToggleClick}>{isToggleOn ? 'ON' : 'OFF'}</button>
+      {/* <a href="https://reactapp.ir" onClick={actionLink}>click me</a>
+      <button onClick={handleToggleClick}>{isToggleOn ? 'ON' : 'OFF'}</button> */}
+      <Greeting isLoggedIn={isLogedIn} />
+      <button onClick={handleLoginClick}>{isLogedIn ? 'LogOut' : 'LogIn'}</button>
     </div>
   );
 }
