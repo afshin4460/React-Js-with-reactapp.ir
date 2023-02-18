c8182cf42b32f567f678b327052ca528efa2d9c9
Afshin <afshin.m4460@gmail.com>
2023-02-18 12:56:40 +0330

    handling event

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 //import Website from './components/webstie';
-import Counter from './components/counter/counter';
+//import Counter from './components/counter/counter';
 
 function App() {
 
-  /* const handleClick = (name) => {
+  /* const handleClick = (name) => { //for Website component
     alert('Hi ' + name)
   }; */
+  const actionLink = (e) => {
+    e.preventDefault();
+    alert('clicked');
+  };
+  const [isToggleOn, setIsToggleOn] = useState(true);
+  const handleToggleClick = () => {
+    setIsToggleOn(!isToggleOn);
+  };
 
   return (
     <div className='App'>
       {/* <Website name='Afshin' age={28} click={() => handleClick('Afshin')} />
       <Website name='shiva' age={27} click={() => handleClick('Shiva')} /> */}
-      <Counter />
+      {/* <Counter /> */}
+      <a href="https://reactapp.ir" onClick={actionLink}>click me</a>
+      <button onClick={handleToggleClick}>{isToggleOn ? 'ON' : 'OFF'}</button>
     </div>
   );
 }
